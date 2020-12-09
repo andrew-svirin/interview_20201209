14cfef24f1eb4326c4d7ffabeb0b7ddf18709fbf
Andriy Svirin <andriy.svirin@ukrinsoft.com>
2020-12-09 08:16:38 -0500

    Update container class. Update CI settings.

--- app/Services/Container.php
+++ app/Services/Container.php
@@ -2,10 +2,33 @@
 
 namespace AndrewSvirin\Interview\Services;
 
+use Psr\Container\ContainerInterface;
+
 /**
  * Contains IoC services dependencies.
  */
-class Container
+class Container implements ContainerInterface
 {
 
+    /**
+     * @inheritDoc
+     *
+     * If service will not be found in container, then create new one.
+     *
+     * @param string $id The class name of service.
+     */
+    public function get($id)
+    {
+        // TODO: Implement get() method.
+    }
+
+    /**
+     * @inheritDoc
+     *
+     * @param string $id The class name of service.
+     */
+    public function has($id)
+    {
+        // TODO: Implement has() method.
+    }
 }
